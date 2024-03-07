dbdb1ccdc0a1ecdc2ae7ad73d1b32dd7ba5b3bcb
Andrei Yeutukhou <gavnorock84@hotmail.com>
2024-03-07 19:15:48 +0300

    prepare template for project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import NavBar from './Components/NavBar/NavBar.tsx'
+
+import './App.module.scss'
+
+const App = () => {
+
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,47 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react'
+import AcmeLogo from './assets/images/AcmeLogo.tsx'
+
+import styles from '../../App.module.scss'
+const NavBar = () => {
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <AcmeLogo />
+        <p className="font-bold text-inherit">Code Asharam</p>
+      </NavbarBrand>
+      <NavbarContent className="sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About Us
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Delivery And Payment
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contacts
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem className={styles.navBar__userPanel}>
+          <Button as={Link} color="primary" variant="solid">
+            Sign In
+          </Button>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
+
+export default NavBar

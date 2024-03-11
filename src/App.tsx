b7a0a5a5a39278e90f34715739654702d85722dd
Andrei Yeutukhou <gavnorock84@hotmail.com>
2024-03-11 20:38:19 +0300

    add react-intl

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
 import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react'
+import { IntlProvider } from 'react-intl'
 
-import NavBar from './Components/NavBar/NavBar.tsx'
 import Container from './Components/UI/Container.tsx'
+import NavBar from './Components/NavBar'
+
+import ru from './translations/ru.json'
+import en from './translations/en.json'
 
 import './App.module.scss'
 
 enum Theme {
   Light = 'light',
   Dark = 'dark'
+}
+
+const languages = {
+  en: en,
+  ru: ru
 }
 
 const App = () => {
@@ -26,41 +35,49 @@ const App = () => {
     )
   }
 
+  // const handleSwitchLanguage = () => {
+  //
+  // }
+
   return (
-    <main className={`${theme} text-foreground bg-background`}>
-      <NavBar onSwitch={handleSwitchColorTheme} isActive={theme === Theme.Dark}/>
-      <Container>
-        <Table aria-label="Example static collection table">
-          <TableHeader>
-            <TableColumn>NAME</TableColumn>
-            <TableColumn>ROLE</TableColumn>
-            <TableColumn>STATUS</TableColumn>
-          </TableHeader>
-          <TableBody>
-            <TableRow key="1">
-              <TableCell>Tony Reichert</TableCell>
-              <TableCell>CEO</TableCell>
-              <TableCell>Active</TableCell>
-            </TableRow>
-            <TableRow key="2">
-              <TableCell>Zoey Lang</TableCell>
-              <TableCell>Technical Lead</TableCell>
-              <TableCell>Paused</TableCell>
-            </TableRow>
-            <TableRow key="3">
-              <TableCell>Jane Fisher</TableCell>
-              <TableCell>Senior Developer</TableCell>
-              <TableCell>Active</TableCell>
-            </TableRow>
-            <TableRow key="4">
-              <TableCell>William Howard</TableCell>
-              <TableCell>Community Manager</TableCell>
-              <TableCell>Vacation</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Container>
-    </main>
+    <>
+      <IntlProvider locale={'ru'} messages={languages.ru}>
+        <main className={`${theme} text-foreground bg-background`}>
+          <NavBar onSwitch={handleSwitchColorTheme} isActive={theme === Theme.Dark} />
+          <Container>
+            <Table aria-label="Example static collection table">
+              <TableHeader>
+                <TableColumn>NAME</TableColumn>
+                <TableColumn>ROLE</TableColumn>
+                <TableColumn>STATUS</TableColumn>
+              </TableHeader>
+              <TableBody>
+                <TableRow key="1">
+                  <TableCell>Tony Reichert</TableCell>
+                  <TableCell>CEO</TableCell>
+                  <TableCell>Active</TableCell>
+                </TableRow>
+                <TableRow key="2">
+                  <TableCell>Zoey Lang</TableCell>
+                  <TableCell>Technical Lead</TableCell>
+                  <TableCell>Paused</TableCell>
+                </TableRow>
+                <TableRow key="3">
+                  <TableCell>Jane Fisher</TableCell>
+                  <TableCell>Senior Developer</TableCell>
+                  <TableCell>Active</TableCell>
+                </TableRow>
+                <TableRow key="4">
+                  <TableCell>William Howard</TableCell>
+                  <TableCell>Community Manager</TableCell>
+                  <TableCell>Vacation</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Container>
+        </main>
+      </IntlProvider>
+    </>
   )
 }
 
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react'
 import { FC } from 'react'
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react'
+import { FormattedMessage } from 'react-intl/lib'
 
 import AcmeLogo from './assets/images/AcmeLogo.tsx'
 import ThemeSwitcher from './parts/ThemeSwitcher/ThemeSwitcher.tsx'
@@ -11,7 +12,7 @@ type Props = {
   isActive: boolean
 }
 
-const NavBar: FC<Props> = ({onSwitch, isActive}) => {
+const NavBar: FC<Props> = ({ onSwitch, isActive }) => {
 
   return (
     <Navbar isBordered maxWidth="2xl">
@@ -22,32 +23,34 @@ const NavBar: FC<Props> = ({onSwitch, isActive}) => {
       <NavbarContent className="sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="#">
-            About Us
+            <FormattedMessage id={"navBar.link.about"}/>
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
           <Link href="#" aria-current="page">
-            Delivery And Payment
+            <FormattedMessage id={"navBar.link.delivery"}/>
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-            Contacts
+            <FormattedMessage id={"navBar.link.contacts"}/>
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="#">
+
+          </Link>
         </NavbarItem>
         <NavbarItem className={styles.navBar__userPanel}>
-          <ThemeSwitcher onSwitch={onSwitch} isActive={isActive}/>
+          <ThemeSwitcher onSwitch={onSwitch} isActive={isActive} />
 
           <Button as={Link} color="primary" variant="solid">
-            Sign In
+            <FormattedMessage id={"navBar.link.signOut"}/>
           </Button>
           <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
+            <FormattedMessage id={"navBar.link.signIn"}/>
           </Button>
         </NavbarItem>
       </NavbarContent>
